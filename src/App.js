e26626d9b15bfa2987eef73f4afe35c409269d6c
nmnhat <nmnhat@tma.com.vn>
2022-05-12 17:13:29 +0700

    create react note app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [textValue, setTextValue] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(()=>{
+    const data = JSON.parse(localStorage.getItem("myNote"));
+    if(data){console.log(data);
+      setTaskList(data)
+    }
+  },[])
+
+  const handleSave = () => {
+    if(textValue) {
+      let temp = [...taskList];
+      temp.push(textValue);
+      setTaskList(temp);
+      setTextValue('');
+    }
+  }
+
+
+  const handleDeleteTask = (task) => {
+    taskList.forEach((el, index) => {
+      if(el === task) {
+        let temp = [...taskList];
+        temp.splice(index, 1);
+        setTaskList(temp);
+      }
+    });
+  }
+
+  const handleDeleteAll = () => {
+    let temp = [...taskList];
+    temp = [];
+    setTaskList(temp);
+  }
+
+  const remember = () => {
+    localStorage.clear();
+    localStorage.setItem('myNote', JSON.stringify(taskList))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text"  placeholder='type task here' value={textValue} onChange={(e) => setTextValue(e.target.value)}/>
+      <div></div>
+      {
+        taskList && taskList.length > 0 && taskList.map((task, index) => (
+          <div key={index} style={{display:'flex'}}>
+            <h2>{task}</h2>
+            <button onClick={() => handleDeleteTask(task)}>delete</button>
+          </div>
+        ))
+      }
+      <button onClick={() => handleSave()}>Save</button>
+      <button onClick={() => handleDeleteAll()}>Delete All</button>
+      <button onClick={() => remember()}>Remember</button>
     </div>
   );
 }
